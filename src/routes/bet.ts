13b3ec639fab28250bf56573ef08d32b95c20a65
Michael Lee <michaellee@ip-172-31-99-132.ec2.internal>
2018-09-30 22:05:07 -0400

    basic functionality working with bets

--- src/routes/bet.ts
+++ src/routes/bet.ts
@@ -1,9 +1,56 @@
 import {Router, Response, Request} from 'express';
+import betService from '../services/betService';
+import betDao from '../dao/betDao';
+import bookDao from '../dao/bookDao';
+import userDao from '../dao/userDao';
+import {WebClient} from '@slack/client';
+
+const token =
+	process.env.SLACK_BOT_TOKEN ||
+	'xoxb-443707066544-446365532997-WYw1gggyIlMHzsZikfLiZZIp';
+const web = new WebClient(token, {
+	clientId: process.env.SLACK_CLIENT_ID,
+	clientSecret: process.env.SLACK_CLIENT_SECRET,
+});
+
 const router: Router = Router();
 
-router.post('/', (req: Request, res: Response) => {
-	console.log(req.body);
-	res.send('ok');
+router.post('/', async (req: Request, res: Response) => {
+	const betData: Book.bet = betService.createNewBet(req.body);
+	await betDao.putBet(betData.id, betData);
+	await bookDao.addToUserBook(betData.bettor_id, betData);
+	const userData: User.info = await userDao.getUserByName(betData.taker);
+	if (betData.taker) {
+		// send confirmation to tagged user
+		await web.chat.postMessage({
+			channel: userData.user_id,
+			text: `New Wager: ${betService.generateWagerText(betData)}`,
+			as_user: true,
+			attachments: [
+				{
+					text: 'Accept or decline wager',
+					fallback: 'You are unable to choose a action',
+					callback_id: betData.id,
+					color: '#3AA3E3',
+					actions: [
+						{
+							name: 'action',
+							text: 'Accept',
+							type: 'button',
+							value: betData.meta.channel_id,
+						},
+						{
+							name: 'action',
+							text: 'Decline',
+							type: 'button',
+							value: 'false',
+						},
+					],
+				},
+			],
+		});
+	}
+	res.send(betService.generateResponseText(betData));
 });
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import {Application} from 'express';
 import bet from './bet';
+import action from './action';
+import bookie from './bookie';
+import register from './register';
 
 const routers = (app: Application): void => {
 	app.use('/bet', bet);
+	app.use('/action', action);
+	app.use('/bookie', bookie);
+	app.use('/register', register);
 };
 
 export default routers;
